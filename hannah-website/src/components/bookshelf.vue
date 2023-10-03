09e037ad256de0e0a64f1addf09820c36f583a10
hannahhannah20 <123084578+hannahhannah20@users.noreply.github.com>
2023-10-03 18:39:50 +0200

    commit 02.10. the second

--- hannah-website/src/components/bookshelf.vue
+++ hannah-website/src/components/bookshelf.vue
@@ -1,17 +1,21 @@
 <template>
-
+    <div class="ball_headline">
+        <Roundball class="roundball" />
+        <h1 class="skills_headline">MY SKILLS</h1>
+    </div>
     <div class="shelf_container">
-        <div class="ball_headline">
-           <Roundball class="roundball" />
-            <h1 class="skills_headline">MY SKILLS</h1>
-        </div>
+        <div class="bookshelf_container">
         <h2 class="skills_h2">Click on the colorful books to learn <br>more about my skills</h2>
-        <p id="skill" :style="{display:showSkill ? 'block' : 'none'}">{{bookTextToShow}}</p>
         <img class="bookshelf" :src="BookshelfImage" alt="Bookshelf">
-        <img class="book book_blue" :src="BlueBookImage" v-on:click="showText('blue')" alt="Blue Book">
-        <img class="book book_neon" :src="NeonBookImage" v-on:click="showText('neon')" alt="Neon Book">
-        <img class="book book_pink" :src="PinkBookImage" v-on:click="showText('pink')" alt="Pink Book">
-        <img class="book book_orange" :src="OrangeBookImage" v-on:click="showText('orange')" alt="Orange Book">
+            <img class="book book_blue" :src="showSkill && bookTextToShow === bookText.blue ? bookAnimations.blue :
+            BlueBookImage" alt="Blue Book" @click="playGif('blue')">
+            <img class="book book_neon" :src="NeonBookImage" @click="showText('neon', $event)" alt="Neon Book">
+        <img class="book book_pink" :src="PinkBookImage" v-on:click="showText('pink', $event)" alt="Pink Book">
+        <img class="book book_orange" :src="OrangeBookImage" v-on:click="showText('orange', $event)" alt="Orange Book">
+        </div>
+        <div class="text_container">
+        <p id="skill" :style="{display:showSkill ? 'block' : 'none'}" v-html="bookTextToShow"></p>
+        </div>
         <h3 class="color">Color</h3>
     </div>
 </template>
@@ -19,9 +23,14 @@
 <script>
 import BookshelfImage from "@/assets/images/Bookshelf.png";
 import BlueBookImage from "@/assets/images/Book-blue.png";
+import BlueBookAnimation from "@/assets/animations/Book-blue-animation.gif";
 import NeonBookImage from "@/assets/images/Book-neon.png";
+import NeonBookAnimation from "@/assets/animations/Book-neon-animation.gif";
 import PinkBookImage from "@/assets/images/Book-pink.png";
+import PinkBookAnimation from "@/assets/animations/Book-pink-animation.gif";
 import OrangeBookImage from "@/assets/images/Book-orange.png";
+import OrangeBookAnimation from "@/assets/animations/Book-orange-animation.gif";
+
 import Roundball from "@/components/roundball.vue";
 
 export default {
@@ -36,35 +45,74 @@ export default {
             NeonBookImage: NeonBookImage,
             PinkBookImage: PinkBookImage,
             OrangeBookImage: OrangeBookImage,
-            showSkill: true,
+            showSkill: false,
+            bookAnimations: {
+                blue: BlueBookAnimation,
+                neon: NeonBookAnimation,
+                pink: PinkBookAnimation,
+                orange: OrangeBookAnimation,
+            },
             bookTextToShow: "",
             bookText: {
-                blue: "Programming Languages: JavaScript\n" +
+                blue: "<span style='font-size: 26px; color: #007ca5;'>Programming Languages:</span>\n" +
+                    "<span style='color: #007ca5;'>JavaScript\n" +
                     "HTML\n" +
                     "CSS\n" +
                     "SQL\n" +
                     "C#\n" +
                     "Python\n" +
-                    "Kotlin",
-                neon: "My vibrant personality.",
-                pink: "Design and Creativity: Figma\n" +
+                    "Kotlin</span>",
+
+                neon: "<span style='font-size: 26px; color: #F505EF;'>My vibrant personality.</span>",
+
+                pink: "<span style='font-size: 26px; color: #E01073;'>Design and Creativity:</span>\n" +
+                    "<span style='color: #E01073;'>Figma\n" +
                     "Adobe Creative Cloud\n" +
-                    "Design Conception",
-                orange: "Web Development:Node.js\n" +
+                    "Design Conception</span>",
+
+                orange: "<span style='font-size: 26px; color: #CE3800;'>Web Development:</span>\n" +
+                    "<span style='color: #CE3800;'>Node.js\n" +
                     "Vue\n" +
                     "React\n" +
                     "Rest API\n" +
-                    "Git",
+                    "Git</span>",
 
-            }
+        }
+    }
+    },
+
+
+    methods: {
+        showText(bookColor, event) {
+
+            this.bookTextToShow = this.bookText[bookColor];
+            this.showSkill = true;
+
+            event.stopPropagation()
+        },
+
+        hideText() {
+            this.showSkill = false;
+
+        },
+
+        playGif(bookColor) {
+            this.bookTextToShow = this.bookAnimations[bookColor];
+            this.showSkill = true;
+
+            setTimeout(() => {
+                this.showSkill=false;
+                this.bookTextToShow="";
+            }, 3000)
         }
     },
 
-    methods: {
-        showText(bookColor) {
-            this.bookTextToShow = this.bookText[bookColor];
-            this.showSkill = true;
-        }
+        mounted() {
+             window.addEventListener('click', this.hideText)
+    },
+
+        beforeDestroy() {
+             window.removeEventListener('click', this.hideText)
     }
 
 }
@@ -74,6 +122,13 @@ export default {
 
 <style>
 
+.shelf_container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+}
+
 .color {
     margin-top: 300px;
     color: transparent;
@@ -82,10 +137,11 @@ export default {
 
 #skill {
     font-family: 'Narnoor', sans-serif;
-    color: whitesmoke;
-    font-weight: lighter;
-    font-size: 80px;
+    font-weight: bold;
+    font-size: 22px;
     margin-left: 30px;
+    white-space: pre-line;
+    text-align: center;
 }
 
 .ball_headline {
