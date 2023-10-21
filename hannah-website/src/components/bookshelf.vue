763b39af530aaa13809e4242358acb6257752ca9
hannahhannah20 <123084578+hannahhannah20@users.noreply.github.com>
2023-10-21 14:16:15 +0200

    commit 21.10.

--- hannah-website/src/components/bookshelf.vue
+++ hannah-website/src/components/bookshelf.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="container">
-        <div class="ball_headline">
+        <div class="headline_text_multiple">
+         <div class="ball_headline">
+             <div class="round"></div>
             <Roundball class="roundball" />
             <h1 class="skills_headline">{{$t('mySkills')}}</h1>
+         </div>
+          <h2 class="skills_h2" v-html="$t('clickBooksToLearn')"></h2>
         </div>
-        <h2 class="skills_h2" v-html="$t('clickBooksToLearn')"></h2>
-
         <div class="shelf_container">
             <div class="bookshelf_container">
                 <img class="bookshelf" :src="BookshelfImage" alt="Bookshelf">
@@ -19,32 +21,35 @@
                 />
                 <img
                     class="book book_neon"
-                    :src="playAnimation('neon') ? bookAnimations.neon : NeonBookImage"
+                    :class="isNeonBookActive ? 'animate' : ''"
+                    :src="NeonBookImage"
                     alt="Neon Book"
-                    @click="playGif('neon');
-                    showText('neon');
+                    @click="setNeonBookActive();
+                    showText('neon');"
 
                 />
 
                 <img
                     class="book book_pink"
-                    :src="playAnimation('pink') ? bookAnimations.pink : PinkBookImage"
-                    @click="playGif('pink');
-                    showText('pink');
-                    disappearBook('pink')"
+                    :class="isPinkBookActive ? 'animate' : ''"
+                    :src="PinkBookImage"
                     alt="Pink Book"
+                    @click="setPinkBookActive();
+                    showText('pink');"
+
                 />
 
                 <img
                     class="book book_orange"
-                    :src="playAnimation('orange') ? bookAnimations.orange : OrangeBookImage"
-                    @click="playGif('orange');
-                    showText('orange');
-                    disappearBook('orange')"
+                    :class="isOrangeBookActive ? 'animate' : ''"
+                    :src="OrangeBookImage"
                     alt="Orange Book"
+                    @click="setOrangeBookActive()
+                    showText('orange');"
+
                 />
             </div>
-            <div class="text_container">
+             <div class="text_container">
                 <p id="skill" :style="{display:showSkill? 'block' : 'none'}" v-html="bookTextToShow"></p>
             </div>
         </div>
@@ -54,16 +59,11 @@
 <script>
 import BookshelfImage from "@/assets/images/Bookshelf.png";
 import BlueBookImage from "@/assets/images/Book-blue.png";
-import BlueBookAnimation from "@/assets/animations/Book-blue-animation.gif";
 import NeonBookImage from "@/assets/images/Book-neon.png";
-import NeonBookAnimation from "@/assets/animations/Book-neon-animation.gif";
 import PinkBookImage from "@/assets/images/Book-pink.png";
-import PinkBookAnimation from "@/assets/animations/Book-pink-animation.gif";
 import OrangeBookImage from "@/assets/images/Book-orange.png";
-import OrangeBookAnimation from "@/assets/animations/Book-orange-animation.gif";
-
 import Roundball from "@/components/roundball.vue";
-import { getTransitionRawChildren } from "vue";
+
 
 export default {
     name: "Bookshelf",
@@ -81,20 +81,7 @@ export default {
             isPinkBookActive: false,
             isNeonBookActive: false,
             isOrangeBookActive: false,
-            showBook: {
-                blue: true,
-                neon: true,
-                pink: true,
-                orange: true,
-            },
             showSkill: false,
-            bookAnimations: {
-                blue: BlueBookAnimation,
-                neon: NeonBookAnimation,
-                pink: PinkBookAnimation,
-                orange: OrangeBookAnimation,
-            },
-            playingAnimation: null,
             showingBookText: false,
             bookTextToShow: "",
             bookText: {
@@ -131,7 +118,6 @@ export default {
 
         methods: {
         showText(bookColor) {
-            console.log("showText called with bookColor:", bookColor);
             this.bookTextToShow = this.bookText[bookColor];
             this.showSkill = true;
             this.showingBookText = true;
@@ -142,37 +128,29 @@ export default {
             this.isBlueBookActive = !this.isBlueBookActive;
             this.isPinkBookActive = false;
             this.isOrangeBookActive = false;
+            this.isNeonBookActive = false;
 
         },
 
-        disappearBook(bookColor) {
-            console.log("disappearBook called with bookColor:", bookColor);
-            if (bookColor === "blue") {
-                this.showBook.blue = false;
-            } else if (bookColor === "neon") {
-                this.showBook.pink = false;
-            } else if (bookColor === "pink") {
-                this.showBook.neon = false;
-            } else if (bookColor === "orange") {
-                this.showBook.orange = false;
-            }
-        },
-
-            playAnimation(bookColor) {
-                return this.playingAnimation !== null && this.playingAnimation.includes(bookColor);
+            setPinkBookActive() {
+                this.isPinkBookActive = !this.isPinkBookActive;
+                this.isBlueBookActive = false;
+                this.isOrangeBookActive = false;
+                this.isNeonBookActive = false;
             },
 
+            setNeonBookActive() {
+                this.isNeonBookActive = !this.isNeonBookActive;
+                this.isBlueBookActive = false;
+                this.isPinkBookActive = false;
+                this.isOrangeBookActive = false;
+            },
 
-
-
-            playGif(bookColor) {
-                console.log("playGif called with bookColor:", bookColor);
-                if (!this.playingAnimation) {
-                    this.playingAnimation = this.bookAnimations[bookColor];
-                    setTimeout(() => {
-                        this.playingAnimation = null;
-                    }, 1400);
-                }
+            setOrangeBookActive() {
+                this.isOrangeBookActive = !this.isOrangeBookActive;
+                this.isBlueBookActive = false;
+                this.isPinkBookActive = false;
+                this.isNeonBookActive = false;
             },
 
             hideText() {
@@ -196,23 +174,60 @@ export default {
 
 <style>
 
+.text_container {
+    align-items: center;
+}
+
+.ball_headline {
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    align-items: stretch;
+    gap: 1%;
+
+}
+
+.skills_headline {
+    font-family: 'Narnoor', sans-serif;
+    color: whitesmoke;
+    font-weight: lighter;
+    font-size: 28px;
+    margin: 0;
+
+}
+
+
+.headline_text_multiple {
+    margin: 2% 4%;
+
+
+}
+
+.skills_h2 {
+    font-family: 'Narnoor', sans-serif;
+    color: whitesmoke;
+    font-weight: lighter;
+    font-size: 20px;
+}
+
 .shelf_container {
     display: flex;
     flex-direction: row;
     align-items: center;
     margin-top: 0;
+    max-width: 1500px;
 
 }
 
 .bookshelf_container {
     position: relative;
-    margin-top: 4rem;
-    width: 100%; 
+    margin: 4rem 2rem;
+    width: 100%;
 }
 
 @media screen and (min-width: 768px) {
     .bookshelf_container {
-        width: 40%;
+        width: 30%;
     }
 }
 
@@ -234,89 +249,132 @@ export default {
     font-size: 26px;
 }
 
-.ball_headline {
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    margin-top: 30px;
-}
-
-.roundball {
-    margin-left: 70px;
-    margin-top: 30px;
-    margin-bottom: 0;
-}
-
-.skills_headline {
-    font-family: 'Narnoor', sans-serif;
-    color: whitesmoke;
-    font-weight: lighter;
-    font-size: 28px;
-    margin-left: 30px;
-
-}
-
-.skills_h2 {
-    font-family: 'Narnoor', sans-serif;
-    color: whitesmoke;
-    font-weight: lighter;
-    font-size: 20px;
-    margin-left: 80px;
-}
-
-.text_container {
-    margin-left: 300px;
-}
-
-
 .book {
     position: absolute;
 }
 
-.book_neon {
-    top: 1250px;
-    left: 200px;
-
-}
-
-.book_pink {
-    top: 1370px;
-    left: 300px;
-}
-
-.book_orange {
-    top: 1110px;
-    left: 250px;
-    width: 700px;
-    height: 110px;
-
-}
-
-/* Je nachdem ob du andere rotationen von den büchern hast, brauchst du eine eigene animations abfolge  */
-/* Idee: zwei animationen 1. raus fahren; 2. shake mit delay */
 @keyframes shake-book {
-    0% {transform: translateX(0) rotate(20deg);}
-    50% {transform: translateX(50%) rotate(20deg);}
-    60% {transform: translateX(50%) rotate(30deg);}
-    70% {transform: translateX(50%) rotate(40deg);}
-    100% {transform: translateX(50%) rotate(20deg);}
+    0% {
+        transform: translateX(0%) rotate(0deg);
+    }
+    2% {
+        transform: translateX(20%) rotate(0deg);
+    }
+    4% {
+        transform: translateX(40%) rotate(0deg);
+    }
+
+    5% {
+        transform: translateX(50%) rotate(0deg);
+    }
+
+    10% {
+        transform: translateX(50%) rotate(-20deg);
+    }
+    15% {
+        transform: translateX(50%) rotate(-40deg);
+    }
+    20% {
+        transform: translateX(50%) rotate(-60deg);
+    }
+    25% {
+        transform: translateX(50%) rotate(-40deg);
+    }
+    30% {
+        transform: translateX(50%) rotate(-20deg);
+    }
+    35% {
+        transform: translateX(50%) rotate(0deg);
+    }
+    40% {
+        transform: translateX(50%) rotate(20deg);
+    }
+    45% {
+        transform: translateX(50%) rotate(40deg);
+    }
+    50% {
+        transform: translateX(50%) rotate(60deg);
+    }
+    55% {
+        transform: translateX(50%) rotate(40deg);
+    }
+    60% {
+        transform: translateX(50%) rotate(20deg);
+    }
+    65% {
+        transform: translateX(50%) rotate(0deg);
+    }
+    70% {
+        transform: translateX(40%) rotate(0deg);
+    }
+    75% {
+        transform: translateX(30%) rotate(0deg);
+    }
+    80% {
+        transform: translateX(20%) rotate(0deg);
+    }
+    85% {
+        transform: translateX(10%) rotate(0deg);
+    }
+    90% {
+        transform: translateX(0%) rotate(0deg);
+    }
+    100% {
+        transform: translateX(0%) rotate(0);
+    }
+}
+
+.book.animate {
+    animation-name: shake-book;
+    animation-duration: 1.7s;
+    animation-iteration-count: 1;
+    animation-timing-function: linear;
 }
 
 .book_blue {
     width: 15%;
     height: auto;
-    top: 13%;
-    left: 55.5%;
-    transform: rotate(20deg) translateX(0);
-    /* transition: transform 1s; */
+    top: 13.5%;
+    left: 43.5%;
 }
 
-.book.animate {
-    /* transform: translateX(50%) rotate(50deg); */
-    animation-name: shake-book;
-    animation-duration: 2s;
-    animation-iteration-count: 1;
-    animation-fill-mode: forwards;
+.book_neon {
+        width: 15%;
+        top: 71%;
+        left: 51%;
+        height: auto;
+
 }
+
+.book_pink {
+    width: 15%;
+    top: 54%;
+    left: 23%;
+    height: auto;
+
+
+}
+
+.book_orange {
+    width: 15%;
+    height: auto;
+    top: 34%;
+    left: 32%;
+
+}
+
+@media screen and (min-width: 330px) and (max-width: 768px) {
+
+    .shelf_container {
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .skills_h2 {
+        font-size:1rem;
+    }
+
+}
+
 
 </style>
